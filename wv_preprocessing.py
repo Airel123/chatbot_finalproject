1ce747ba8df684b42c5244aa3c5bbcf4ba872ba5
Airel123 <121603267+Airel123@users.noreply.github.com>
2024-04-03 23:33:20 +0800

    Update wv_preprocessing.py
    integrate with old preprocessing

--- wv_preprocessing.py
+++ wv_preprocessing.py
@@ -1,95 +1,117 @@
 import csv
-import re, nltk, random, time
-from nltk.corpus import stopwords
+import re, nltk, random
 import numpy as np
-import torch
 from sklearn.model_selection import train_test_split
 from gensim.models import Word2Vec
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import Dataset, DataLoader
 
-
-# 假设 tokenize 和 filter_sent 函数已经定义
 
 class Corpus:
-    def __init__(self, filePath, maxSentenceWordsNum=100, vector_size=100, window=5, min_count=1, workers=4):
-        self.cleaned_patterns = []  # 用于存储第一列清理后数据
-        self.cleaned_responses = []  # 用于存储第二列清理后数据
+    def __init__(self, filePath, maxSentenceWordsNum=100, id2word=None, word2id=None, wordNum=None, vector_size=100,
+                 window=5, min_count=1, workers=4, testSize=0.15):
+
+        self.id2word, self.word2id, self.wordNum = id2word, word2id, wordNum
+        cleaned_patterns = []  # 用于存储第一列清理后数据
+        cleaned_responses = []  # 用于存储第二列清理后数据
         with open(filePath, 'r', encoding='utf8', newline='') as file:
             reader = csv.reader(file)
             for row in reader:
                 if len(row) >= 2:
-                    self.cleaned_patterns.append(filter_sent(row[0]))
-                    self.cleaned_responses.append(filter_sent(row[1]))
+                    cleaned_patterns.append(filter_sent(row[0]))
+                    cleaned_responses.append(filter_sent(row[1]))
                 else:
                     print(f"Skipped a line with unexpected number of columns: {row}")
 
-        self.tokenized_patterns = [tokenize(sentence) for sentence in self.cleaned_patterns]
-        self.tokenized_responses = [tokenize(sentence) for sentence in self.cleaned_responses]
+        tokenized_patterns = [tokenize(sentence) for sentence in cleaned_patterns]
+        tokenized_responses = [tokenize(sentence) for sentence in cleaned_responses]
 
         # 添加特殊标识符
         self.tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
-        for i in range(len(self.tokenized_patterns)):
-            self.tokenized_patterns[i] = ['<SOS>'] + self.tokenized_patterns[i] + ['<EOS>']
+        for i in range(len(tokenized_patterns)):
+            tokenized_patterns[i] = ['<SOS>'] + tokenized_patterns[i] + ['<EOS>']
 
-        for i in range(len(self.tokenized_responses)):
-            self.tokenized_responses[i] = ['<SOS>'] + self.tokenized_responses[i] + ['<EOS>']
+        for i in range(len(tokenized_responses)):
+            tokenized_responses[i] = ['<SOS>'] + tokenized_responses[i] + ['<EOS>']
 
         # 构建问答对列表
         data = []
-        for p, r in zip(self.tokenized_patterns, self.tokenized_responses):
+        for p, r in zip(tokenized_patterns, tokenized_responses):
             if len(p) < maxSentenceWordsNum and len(r) < maxSentenceWordsNum:
                 data.append(p)
                 data.append(r)
 
         self.chatDataWord = data
-        self.totalSampleNum = len(data)
-        print("Total qa pairs num:", self.totalSampleNum)
 
         # Train Word2Vec model
         self.model = Word2Vec(sentences=self.chatDataWord, vector_size=vector_size, window=window, min_count=min_count,
                               workers=workers)
-        print("word2vec model built!")
+        self.model.save("word2vec.model")
+        print("word2vec model built and saved!")
+
 
         self._word_id_map()
 
         try:
             chatDataId = [[[self.word2id[w] for w in qa[0]], [self.word2id[w] for w in qa[1]]] for qa in
                           self.chatDataWord]
-        except:
-            chatDataId = [[[self.word2id[w] for w in qa[0] if w in self.id2word],
-                           [self.word2id[w] for w in qa[1] if w in self.id2word]] for qa in self.chatDataWord]
+        except KeyError:
+            chatDataId = [[[self.word2id.get(w, self.word2id['<UNK>']) for w in qa[0]],
+                           [self.word2id.get(w, self.word2id['<UNK>']) for w in qa[1]]] for qa in self.chatDataWord]
 
+        self._QALens(chatDataId)
+        self.maxSentLen = max(maxSentenceWordsNum, self.AMaxLen)
         self.QChatDataId, self.AChatDataId = [qa[0] for qa in chatDataId], [qa[1] for qa in chatDataId]
+        self.totalSampleNum = len(data)
+        print("Total qa pairs num:", self.totalSampleNum)
         self.trainIdList, self.testIdList = train_test_split([i for i in range(self.totalSampleNum)],
-                                                             test_size=0.15)
+                                                             test_size=testSize)
         self.trainSampleNum, self.testSampleNum = len(self.trainIdList), len(self.testIdList)
         print(f"train pairs size: {self.trainSampleNum}; test pairs size: {self.testSampleNum}")
         print("Finished loading corpus!")
 
-    # 构建批次（Batching）
-    def batching(self, batchSize=128, type='train'):
-        # Choose the correct ID list based on the type parameter
+    # Resets mappings between words and IDs.
+    def reset_word_id_map(self, id2word, word2id):
+        self.id2word, self.word2id = id2word, word2id
+        # Update chat data IDs based on new mappings.
+        chatDataId = [[[self.word2id.get(w, self.word2id['<UNK>']) for w in qa[0]],
+                       [self.word2id.get(w, self.word2id['<UNK>']) for w in qa[1]]] for qa in self.chatDataWord]
+        self.QChatDataId, self.AChatDataId = zip(*[(qa[0], qa[1]) for qa in chatDataId])
+
+    # Generates a random batch of data for training or testing.
+    def random_batch_data_stream(self, batchSize=128, type='train'):
         idList = self.trainIdList if type == 'train' else self.testIdList
+        eosToken, unkToken = self.word2id['<EOS>'], self.word2id['<UNK>']
+        while True:
+            # Select a random subset of data for the batch.
+            samples = random.sample(idList, min(batchSize, len(idList))) if batchSize > 0 else idList
+            QMaxLen, AMaxLen = max(self.QLens[samples]), max(self.ALens[samples])
+            # Prepare question and answer data with padding.
+            QDataId = np.array([self.QChatDataId[i] + [eosToken] * (QMaxLen - self.QLens[i] ) for i in samples],
+                               dtype='int32')
+            ADataId = np.array([self.AChatDataId[i] + [eosToken] * (AMaxLen - self.ALens[i] ) for i in samples],
+                               dtype='int32')
+            yield QDataId, self.QLens[samples], ADataId, self.ALens[samples]
 
-        # Initialize batches
-        encoder_batches = []
-        decoder_batches = []
+    # Provides data for a complete epoch, used in evaluating the model.
+    def one_epoch_data_stream(self, batchSize=128, type='train'):
+        idList = self.trainIdList if type == 'train' else self.testIdList
+        eosToken = self.word2id['<EOS>']
+        for i in range((len(idList) + batchSize - 1) // batchSize):
+            # Slice data for current batch.
+            samples = idList[i * batchSize:(i + 1) * batchSize]
+            QMaxLen, AMaxLen = max(self.QLens[samples]), max(self.ALens[samples])
+            # Prepare question and answer data with padding.
+            QDataId = np.array([self.QChatDataId[i] + [eosToken] * (QMaxLen - self.QLens[i] ) for i in samples],
+                               dtype='int32')
+            ADataId = np.array([self.AChatDataId[i] + [eosToken] * (AMaxLen - self.ALens[i] ) for i in samples],
+                               dtype='int32')
+            yield QDataId, self.QLens[samples], ADataId, self.ALens[samples]
 
-        # Loop through the ID list and create batches
-        for i in range(0, len(idList), batchSize):
-            batch_ids = idList[i:i + batchSize]
-            encoder_batch = [torch.tensor(self.QChatDataId[idx], dtype=torch.long) for idx in batch_ids]
-            decoder_batch = [torch.tensor(self.AChatDataId[idx], dtype=torch.long) for idx in batch_ids]
-
-            # Pad sequences in the batch
-            encoder_batch_padded = pad_sequence(encoder_batch, batch_first=True, padding_value=self.word2id['<PAD>'])
-            decoder_batch_padded = pad_sequence(decoder_batch, batch_first=True, padding_value=self.word2id['<PAD>'])
-
-            encoder_batches.append(encoder_batch_padded)
-            decoder_batches.append(decoder_batch_padded)
-
-        return encoder_batches, decoder_batches
+    def _QALens(self, data):
+        QLens, ALens = [len(qa[0]) + 1 for qa in data], [len(qa[1]) + 1 for qa in data]
+        QMaxLen, AMaxLen = max(QLens), max(ALens)
+        print('QMAXLEN:', QMaxLen, '  AMAXLEN:', AMaxLen)
+        self.QLens, self.ALens = np.array(QLens, dtype='int32'), np.array(ALens, dtype='int32')
+        self.QMaxLen, self.AMaxLen = QMaxLen, AMaxLen
 
     def _word_id_map(self):
         # 预先定义特殊词汇的索引
@@ -105,9 +127,19 @@ class Corpus:
                 self.id2word[next_index] = word
                 next_index += 1
         print("Word-ID mapping built!")
-        print(self.word2id)
+        # print(self.word2id)
         self.wordNum = len(self.id2word)
         print('Unique words num:', len(self.id2word) - 4)
+
+
+def seq2id(word2id, seqData):
+    seqId = [word2id[w] for w in seqData]
+    return seqId
+
+
+def id2seq(id2word, seqId):
+    seqData = [id2word[i] for i in seqId]
+    return seqData
 
 
 def tokenize(sentence):
